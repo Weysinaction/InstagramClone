25c99bc601f16342280783edc7ebea06c6ab5b8d
[Weysinaction] <[lipatoff.const@gmail.com]>
2021-07-22 16:05:31 +0300

    сделал изображение сингл скейл

---
+++ TableViewPractice/TableViewPractice/TableViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Владислав Лазарев on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
